388e01b4ee29e5afd0f795ba42b42a42a47d564d
benaiah-ke <benaiahpraise@gmail.com>
2022-11-06 23:34:39 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES
+
+    ('Mr Chocolate',12,'M','Brown','Angry',1),
+    ('Rowdy',12,'M','Brown','Angry',1),
+    ('Tabitha',10,'F','Brown','Happy',1),
+    ('Sergeant Brown',12,'M','White','Happy',1),
+    ('Melissa',15,'F','Black','Angry',1),
+    ('Grinch',12,'M','Brown','Angry',1),
+    ('Wendy',12,'F','Brown','Angry',1),
+    (NULL,12,'M','Black','Sad',1)
+
+;
